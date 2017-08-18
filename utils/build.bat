7bdc9995a21614defaf1cee342173f28ff3069d6
lightyears1998 <lightyears1998@hotmail.com>
2017-08-18 15:26:25 +0800

    添加批量编译批处理

--- utils/build.bat
+++ utils/build.bat
@@ -1,11 +1,14 @@
 @echo off
 goto start
 ====== 注释开始啦 ======
-批量编译批处理
+# 批量编译批处理
+
+将此脚本放入源文件的文件夹中，运行，即可自动编译目录下的所有源文件
 ====== 注释结束啦 ======
 :start
 
 set COMPLIER="gcc.exe"
+rem TODO 支持CL编译器
 set OUTPUT_FOLDER="bin"
 set EXTRA_ARGS="-Wall"
 
