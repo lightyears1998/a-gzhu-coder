2b62c829399301053059b0fb62200de0469a425c
lightyears1998 <lightyears1998@hotmail.com>
2017-11-22 23:58:01 +0800

    添加汉诺塔演示模式

--- freshman/hanoi-tower.cpp
+++ freshman/hanoi-tower.cpp
@@ -1,5 +1,6 @@
 /* 暴力求解汉诺塔问题 */
 #include <iostream>
+#include <cstdio>
 using namespace std;
 
 unsigned long long scheme[70] = {0, 1};
@@ -10,6 +11,17 @@ unsigned long long move(int num)
     else scheme[num] = move(num - 1) + 1 + move(num - 1);
     return scheme[num];
 }
+
+// 经典模式/演示模式：无优化
+// unsigned long long move(int num)
+// {
+//     if (num == 1) return 1;
+//     else {
+//         printf("Want to know move(%d)\tCalculating move(%d - 1)\n", num, num);
+//         printf("Get move(%d - 1) = %lld\n", num, move(num - 1));
+//         return move(num - 1) + 1 + move(num - 1);
+//     }
+// }
 
 int main()
 {
