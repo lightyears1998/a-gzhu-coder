0c9c82c683bb98f9c586dbd068ef12cba41cc8de
lightyears1998 <lightyears1998@hotmail.com>
2017-11-24 14:41:24 +0800

    帮助大炮解决问题

---
+++ acm/cf/#446/Greed.cpp
@@ -0,0 +1,22 @@
+#include <cstdio>
+#include <algorithm>
+using namespace std;
+
+int main()
+{
+    int n;
+    while (scanf("%d", &n) == 1)
+    {
+        long long mx = -1, mx2 = -1, tot = 0;
+
+        for (int i = 1; i < n; i++)
+        {
+
+        }
+
+        for ()
+
+        if (mx + mx2 >= tot) printf("YES\n");
+        else printf("NO\n");
+    }
+}
