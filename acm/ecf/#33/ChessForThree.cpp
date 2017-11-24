d2939193aefe4df81850663bf285544f0a46d1f3
lightyears1998 <lightyears1998@hotmail.com>
2017-11-24 12:09:58 +0800

    Educational CodeForces #33

---
+++ acm/ecf/#33/ChessForThree.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+enum status{win, lose, sp};
+
+int main()
+{
+    int game;
+    while (cin >> game)
+    {
+        int player[4];
+
+        
+
+        game--;
+        while (game--)
+        {
+
+        }
+    }
+}
