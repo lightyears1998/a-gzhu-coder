e8af0e7f8d3bc25f071984707b2af05bc73c6508
lightyears1998 <lightyears1998@hotmail.com>
2017-11-24 17:37:58 +0800

    CF#446 2/5
    Three more to go~

--- acm/cf/#446/Greed.cpp
+++ acm/cf/#446/Greed.cpp
@@ -7,16 +7,24 @@ int main()
     int n;
     while (scanf("%d", &n) == 1)
     {
-        long long mx = -1, mx2 = -1, tot = 0;
+        long long mx = -1, mx2 = -1, vol = 0;
 
-        for (int i = 1; i < n; i++)
+        for (int i = 1; i <= n; i++)
         {
-
+            int tmp; scanf("%d", &tmp);
+            vol += tmp;
         }
 
-        for ()
+        for (int i = 1; i <= n; i++)
+        {
+            long long tmp; scanf("%lld", &tmp);
+            if (tmp >= mx) {
+                swap(mx, mx2);
+                swap(tmp, mx);
+            } else if (tmp > mx2) swap(mx2, tmp);
+        }
 
-        if (mx + mx2 >= tot) printf("YES\n");
+        if (mx + mx2 >= vol) printf("YES\n");
         else printf("NO\n");
     }
 }
