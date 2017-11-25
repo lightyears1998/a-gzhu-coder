c7d243d240ef367fcfe57cce5b2d88fb6339a455
lightyears1998 <lightyears1998@hotmail.com>
2017-11-25 14:05:05 +0800

    解决大炮的一个问题

--- freshman/checker/1125double2.cpp
+++ freshman/checker/1125double2.cpp
@@ -1,32 +1,44 @@
-/* Unsolved */
-#include <string>
-#include <cstring>
-#include <iostream>
-#include <map>
-using namespace std;
+/**
+ * 反例
+ *
+ * INPUT: 1 3 2
+ * OUTPUT:
+ * 3 1
+ * 2 3
+ * 1 2
+ *
+ * 建议模拟一遍选择排序的过程
+**/
 
-bool cmp(int a, int b)
-{
-    return a > b;
-}
-
-int main()
-{
-    int a[2050];
-    int b[2050];
-    int n;
-    while (scanf("%d", &n) == 1)
-    {
-        for (int i = 1; i <= n; i++)
-        {
-            scanf("%d", &a[i]); b[i] = a[i];
-        }
-        sort(a + 1, a + n + 1, cmp);
-        for (int i = 1; i <= n - 1; i++)
-        {
-            if (a[i] >= b[i]) printf("%d %d\n", a[i], b[i]);
-            else printf("%d %d\n", a[i], a[i]);
-         }
-         printf("\n");
-     }
-}
+// #include <string>
+// #include <cstring>
+// #include <iostream>
+// #include <map>
+// using namespace std;
+//
+// bool cmp(int a, int b)
+// {
+//     return a > b;
+// }
+//
+// int main()
+// {
+//     int a[2050];
+//     int b[2050];
+//     int n;
+//     while (scanf("%d", &n) == 1)
+//     {
+//         for (int i = 1; i <= n; i++)
+//         {
+//             scanf("%d", &a[i]);
+//             b[i] = a[i];
+//         }
+//         sort(a + 1, a + 1 +n, cmp);
+//         for (int i = 1; i <= n - 1; i++)
+//         {
+//             if (a[i] >= b[i]) printf("%d %d\n", a[i], b[i]);
+//             else printf("%d %d\n", a[i], a[i]);
+//         }
+//         printf("\n");
+//     }
+// }
