61b3309bca3452f92af21fb146fc9f2723f36264
lightyears1998 <lightyears1998@hotmail.com>
2017-12-07 08:49:04 +0800

    未能帮助city解决问题
    不确定是语法错误还是算法本身存在问题

--- freshman/checker/1206city.cpp
+++ freshman/checker/1206city.cpp
@@ -1,4 +1,5 @@
-/* 挤牛奶 */
+/* Wrong Answer - 用指针排序之后不能得到正确答案，未解之谜，算法存在问题吗？= */
+
 /**
  * 最长一人挤奶时间 - 反例
  *
@@ -29,6 +30,12 @@
 #include<algorithm>
 using namespace std;
 
+bool cmp(long long (*lhs)[2], long long (*rhs)[2])
+{
+	if ((*lhs)[0] != (*rhs)[0]) return (*lhs)[0] < (*rhs)[0];
+	else return (*lhs)[1] < (*rhs)[1];
+}
+
 int main()
 {
 	long long s[6000][2];
@@ -44,15 +51,30 @@ int main()
 			}
 		}
 
+		long long (* ps[6000])[2];
+		for (int i = 0; i < n; i++) ps[i] = &s[i];
+		sort(ps, ps + n, cmp);
+
+		cout << "--------" << endl;
+		for (int i = 0;i < n; i++)
+		{
+			for (int j = 0;j < 2; j++)
+			{
+				cout << (*ps[i])[j] << " ";
+			}
+			cout << endl;
+		}
+		cout << "--------" << endl;
+
 		long long a, b, max = 0;
 		for (int i = 0; i < n; i++)
 		{
-			a = s[i][0];
-			b = s[i][1];
+			a = (*ps[i])[0];
+			b = (*ps[i])[1];
 			for (int j = 0; j < n; j++)
 			{
-				if (s[j][0] < a && a <= s[j][1] && s[j][1] <= b) a = s[j][0];
-				if (s[j][1] > b && a <= s[j][0] && s[j][0] <= b) b = s[j][1];
+				if ((*ps[j])[0] < a && a <= (*ps[j])[1] && (*ps[j])[1] <= b) a = (*ps[j])[0];
+				if ((*ps[j])[1] > b && a <= (*ps[j])[0] && (*ps[j])[0] <= b) b = (*ps[j])[1];
 			}
 			max = (b - a) > max ? (b - a) : max;
 		}
@@ -62,12 +84,12 @@ int main()
 		long long max1 = 0;
 		for (int i = 0; i < n; i++)
 		{
-			h = s[i][0];
-			m = s[i][1];
+			h = (*ps[i])[0];
+			m = (*ps[i])[1];
 			for (int j = 0; j < n; j++)
 			{
-				if (s[j][0] < h && h <= s[j][1] && s[j][1] <= m) h = s[j][0];
-				if (s[j][1] > m && h <= s[j][0] && s[j][0] <= m) m = s[j][1];
+				if ((*ps[j])[0] < h && h <= (*ps[j])[1] && (*ps[j])[1] <= m) h = (*ps[j])[0];
+				if ((*ps[j])[1] > m && h <= (*ps[j])[0] && (*ps[j])[0] <= m) m = (*ps[j])[1];
 			}
 
 			if (x == 0 && y == 0)
