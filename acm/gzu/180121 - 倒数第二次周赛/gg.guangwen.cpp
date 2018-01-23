272e3d89bf855afc50fd46ead1c369eda63f2db4
lightyears1998 <lightyears1998@hotmail.com>
2018-01-23 21:32:53 +0800

    整理格式

--- acm/gzu/180121 - 倒数第二次周赛/gg.guangwen.cpp
+++ acm/gzu/180121 - 倒数第二次周赛/gg.guangwen.cpp
@@ -1,86 +1,86 @@
-// Author£ºÎÒ°®ÂŞ
-#include <iostream>  
-#include <cstring>   
-#include <cstdio>   
-#include <algorithm>  
-#include <cmath>     
-#include <string.h>  
+// Authorï¼šæˆ‘çˆ±ç½—
+#include <iostream>
+#include <cstring>
+#include <cstdio>
+#include <algorithm>
+#include <cmath>
+#include <string.h>
 using namespace std;
 int arr[100050],n;char a[20];
 struct node
 {
-    int val;  //½«ÓÃÀ´´¢´æÄ³Çø¼ä×î´óÖµ
-    int l,r;  //×óÓÒ¶ù×Ó±àºÅ£¨Çø¼ä·¶Î§£© 
+	int val;  //å°†ç”¨æ¥å‚¨å­˜æŸåŒºé—´æœ€å¤§å€¼
+	int l,r;  //å·¦å³å„¿å­ç¼–å·ï¼ˆåŒºé—´èŒƒå›´ï¼‰
 }tree[3000001];
 
-void build(int root,int l,int r)  //½¨Ê÷ 
+void build(int root,int l,int r)  //å»ºæ ‘
 {
-    int mid;
-   
-    tree[root].l=l;tree[root].r=r;
-    if (l==r) tree[root].val=arr[l];
-    else
-    {
-        mid=(l+r)/2;
-        build(root*2,l,mid);     //µİ¹é¹¹Ôì×ó×ÓÊ÷
-        build(root*2+1,mid+1,r); //µİ¹é¹¹ÔìÓÒ×ÓÊı
-        tree[root].val=max(tree[root*2].val,tree[root*2+1].val);//´æ´¢×óÓÒ×ÓÊ÷µÄ×î´óÖµ 
-    }
+	int mid;
+
+	tree[root].l=l;tree[root].r=r;
+	if (l==r) tree[root].val=arr[l];
+	else
+	{
+		mid=(l+r)/2;
+		build(root*2,l,mid);     //é€’å½’æ„é€ å·¦å­æ ‘
+		build(root*2+1,mid+1,r); //é€’å½’æ„é€ å³å­æ•°
+		tree[root].val=max(tree[root*2].val,tree[root*2+1].val);//å­˜å‚¨å·¦å³å­æ ‘çš„æœ€å¤§å€¼
+	}
 }
 
-void update(int root,int ind,int addval)  //µ¥µã¸üĞÂ 
+void update(int root,int ind,int addval)  //å•ç‚¹æ›´æ–°
 {
-    int mid;
-    if (tree[root].l==tree[root].r)
-    {
-        tree[root].val+=addval;
-        return;
-    }
-    else
-    {
-        mid=(tree[root].l+tree[root].r)/2;
-        if (ind<=mid) update(root*2,ind,addval);
-        else update(root*2+1,ind,addval);
-        tree[root].val=max(tree[root*2].val,tree[root*2+1].val);
-    }
+	int mid;
+	if (tree[root].l==tree[root].r)
+	{
+		tree[root].val+=addval;
+		return;
+	}
+	else
+	{
+		mid=(tree[root].l+tree[root].r)/2;
+		if (ind<=mid) update(root*2,ind,addval);
+		else update(root*2+1,ind,addval);
+		tree[root].val=max(tree[root*2].val,tree[root*2+1].val);
+	}
 }
 
 int ask(int root,int l,int r)
 {
-    int mid;
-    if (tree[root].l==l&&tree[root].r==r)
-        return tree[root].val;
-    else 
-    {
-        mid=(tree[root].l+tree[root].r)/2;
-        if (mid>=r)
-            return ask(root*2,l,r);
-        else if (mid<l)
-            return ask(root*2+1,l,r); 
-        else return max(ask(root*2,l,mid),ask(root*2+1,mid+1,r));
+	int mid;
+	if (tree[root].l==l&&tree[root].r==r)
+		return tree[root].val;
+	else
+	{
+		mid=(tree[root].l+tree[root].r)/2;
+		if (mid>=r)
+			return ask(root*2,l,r);
+		else if (mid<l)
+			return ask(root*2+1,l,r);
+		else return max(ask(root*2,l,mid),ask(root*2+1,mid+1,r));
 
-    }
+	}
 }
 
 int main()
 {
 	int t,ans=1;
-   scanf("%d",&t);
-   while(t--)
-   {
-   	scanf("%d",&n);
-   	 for(int i=1;i<=n;i++)
-   	 scanf("%d",&arr[i]);
-   	 build(1,1,n);
-   	 printf("Case %d:\n",ans++);
-   	 int ind,val;
-   	 while(scanf("%s",&a))
-   	 {
-   	 	if(a[0]=='E')break;
-   	 	scanf("%d%d",&ind,&val);
-   	 	if(a[0]=='Q') printf("%d\n",ask(1,ind,val));
-   	 	else if(a[0]=='A')update(1,ind,val);
-   	 	else if(a[0]=='S')update(1,ind,-val);
+	scanf("%d",&t);
+	while(t--)
+	{
+		scanf("%d",&n);
+		for(int i=1;i<=n;i++)
+		scanf("%d",&arr[i]);
+		build(1,1,n);
+		printf("Case %d:\n",ans++);
+		int ind,val;
+		while(scanf("%s",&a))
+		{
+			if(a[0]=='E')break;
+			scanf("%d%d",&ind,&val);
+			if(a[0]=='Q') printf("%d\n",ask(1,ind,val));
+			else if(a[0]=='A')update(1,ind,val);
+			else if(a[0]=='S')update(1,ind,-val);
 		}
    }
 }
