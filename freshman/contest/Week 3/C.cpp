c6f04df82cab01813b7f895824c5efc1e23be3b1
lightyears <lightyears1998@hotmail.com>
2018-03-27 16:48:19 +0800

    Another try in order not to lose...

--- freshman/contest/Week 3/C.cpp
+++ freshman/contest/Week 3/C.cpp
@@ -1,17 +1,35 @@
 #include <bits/stdc++.h>
 using namespace std;
+using LL = long long;
 
 const int maxN = 1010;
-int num[maxN];
-int dp[maxN][2];
+const LL INF = 0x3F3F3F3F3F3F3F3F;
 
-void solve()
-{
-	
-}
+LL num[maxN], sum[maxN];
 
 int main()
 {
-	int n; scanf("%d", &n);
-	
+	int n; 
+	while (scanf("%d", &n) == 1 && n) 
+	{
+		memset(num, 0, sizeof(num));
+		memset(sum, 0, sizeof(sum));
+		
+		num[0] = -INF;
+		for (int i=1; i<=n; ++i) {
+			scanf("%lld", &num[i]); 
+		}
+		
+		for (int i=1; i<=n; ++i) {
+			int amx = 0;
+			for (int j=0; j<i; ++j)	{
+				if (num[j] < num[i] && sum[j]>amx) {
+					amx = sum[j];
+				}
+			}
+			sum[i] = num[i] + amx;
+		} 
+		
+		printf("%lld\n", *max_element(sum, sum+1+n));
+	}
 }
--- freshman/contest/Week 3/F.cpp
+++ freshman/contest/Week 3/F.cpp
@@ -1,5 +1,7 @@
 #include <cstdio>
 #include <algorithm>
+#include <queue> 
+#include <iostream>
 using namespace std;
 
 const int maxN = 10010;
@@ -7,12 +9,16 @@ const int maxN = 10010;
 struct product {
 	int price, deadline;
 	
-	bool operator <(const product &d) const 
-	{
-		if (deadline != d.deadline) return deadline < d.deadline;
+	bool operator <(const product &d) const {
 		return price < d.price;
 	}
 } p[maxN];
+
+bool cmp(const product &a, const product &b) {
+	return a.deadline < b.deadline;
+}
+
+priority_queue<product> que;
 
 int main()
 {
@@ -20,20 +26,27 @@ int main()
 	while (scanf("%d", &n) == 1)
 	{
 		int tailing = 0;
+		while (que.size()) que.pop();
 		
 		for (int i=0; i<n; ++i) {
 			scanf("%d%d", &p[i].price, &p[i].deadline);
 			tailing = max(tailing, p[i].deadline);
 		}
-		sort(p, p+n);
+		sort(p, p+n, cmp);
 		
 		int profit = 0;
-		for (int t=tailing, j=n-1; t>0&&j>=0; ) {
-			if ()
+		for (int t=tailing, j=n-1; t>0; ) {
+			while (j>=0 && p[j].deadline==t) 
+				que.push(p[j]), --j;
 			
+			if (que.size()) {
+				profit += que.top().price;
+				que.pop();
+			} 
 			--t;
 		}
 		
 		printf("%d\n", profit);
 	}
 }
+
