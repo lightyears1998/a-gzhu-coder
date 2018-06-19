a274fb11ef788ba90304920740391aea219fb085
lightyears1998 <lightyears1998@hotmail.com>
2018-06-19 15:52:27 +0800

    改用非线性拟合

--- period/freshman/物理实验/变温粘滞系数的测定/figure.m
+++ period/freshman/物理实验/变温粘滞系数的测定/figure.m
@@ -3,15 +3,20 @@
 nw = [.548, .469, .343, .222, .182];
 t  = [  30,   35,   40,   45,   50];
 
-P = polyfit(t, nw, 2);
+function rslt = fun(var, data)
+  rslt = var(1) * exp(-var(2) * data);
+endfunction
+
+var0 = [0 0];
+P = lsqcurvefit(@fun, var0, t, nw);
 
 graphics_toolkit("gnuplot");
 figure();
 
 hold on;
-grid on;
+grid off;
 axis([25, 55, 0, 0.6]);
 plot(t, nw, '+', 'MarkerSize', 12);
-plot([25 t 55], polyval(P, [25 t 55]), 'LineWidth', 5);
+plot([25:1:55], fun(P, [25:1:55]), 'LineWidth', 5);
 print('figure.png', '-dpng');
 hold off;
